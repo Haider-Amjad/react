b5895e588430dc15d55364b4a59349de43968b3f
Haider-Amjad <haiderajad25@gmail.com>
2020-03-11 10:46:33 +0500

    Updates

---
+++ precache-manifest.7c718889f1d9d4c32c3d1ee8b2b7eb7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7bccdfc84d623f84b220481037c0a2c",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "d46c99522a591fac09aa",
+    "url": "/react/static/css/main.2355212f.chunk.css"
+  },
+  {
+    "revision": "aa3c85476dd53b921f9f",
+    "url": "/react/static/js/2.4f36d905.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react/static/js/2.4f36d905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46c99522a591fac09aa",
+    "url": "/react/static/js/main.3e8c782c.chunk.js"
+  },
+  {
+    "revision": "161512d392d42b16bf2b",
+    "url": "/react/static/js/runtime-main.db5e024b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
